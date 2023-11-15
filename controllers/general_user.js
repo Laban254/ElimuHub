8fc7ae221d4161a098e71432c83378df99ca6ea9
myrajarenga <myrajarenga1234@.com>
2023-11-15 11:54:43 +0300

    Login authenticating user fro adatabase. Querring adatabase email and its password'

--- controllers/general_user.js
+++ controllers/general_user.js
@@ -1,7 +1,20 @@
+const users = require('../models/users.js')
 // This file contains all the controls related to any user of the system
 const home = (req, res) => {
     console.log("Welcome to ElimuHub :)");
     res.end("Welcome to ElimuHub :)");
 }
+const logIn = (req, res ) => {
+  const email = req.body.email;
+  const password = req.body.password;
+// Replace this with your actual user authentication logic (e.g., querying a database)
+  users.findOne({email:email}).then(() => {
+    if (users.password === password)
+      console.log('successful log in')
+    else 
+    console.log('incorrect password')
+  });
 
-module.exports = {home}
+  
+    }
+module.exports = {home, logIn}
