999ac8f5ea2bbf395d716ccd32c3b03d07248069
myrajarenga <myrajarenga1234@.com>
2023-11-15 11:56:40 +0300

    login route

--- routes/general.routes.js
+++ routes/general.routes.js
@@ -1,12 +1,14 @@
 const express = require("express");
-const { home } = require("../controllers/general_user.js");
+const { home, logIn } = require("../controllers/general_user.js");
 const app = express();
 
 // creation of a router object
 const router = express.Router();
-
+router.use(express.json())
 // insert all the main routes associated with the general user under
+
 router.get("/", home);
+router.post('/login', logIn)
 
 module.exports = router;
 
