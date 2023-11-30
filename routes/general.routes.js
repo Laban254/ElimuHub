a6225f2c0864fcee63d13f38a3c54c2ec80bea7e
atonya-bravin <bravinatonya.brani@gmail.com>
2023-11-30 19:50:02 +0300

    configuration of the session link to the router and inclusion of the startSession and sessionValidity routes that are purposed with session generation and validity

--- routes/general.routes.js
+++ routes/general.routes.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const { home } = require("../controllers/general_user.js");
-const app = express();
+const { generateAndPopulateSession, checkSessionValidity} = require('../controllers/powerHouse.js');
+
+const session = require('express-session');
 
 // creation of a router object
 const router = express.Router();
 
+router.use(
+    session({
+      secret: 'ElimuHub-secrets',
+      resave: false,
+      saveUninitialized: true,
+    })
+);
+
 // insert all the main routes associated with the general user under
 router.get("/", home);
+router.get('/startSession', generateAndPopulateSession("userAuthKey", "userEmail", "userPassword", 200));
+router.get('/sessionValidity', checkSessionValidity)
 
 module.exports = router;
 
