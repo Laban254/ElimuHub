75a9c656018f3b408be5168eceb2689c9d5669ed
atonya-bravin <bravinatonya.brani@gmail.com>
2023-11-30 19:47:14 +0300

    use of send instead of end method to display the welcome message

--- controllers/general_user.js
+++ controllers/general_user.js
@@ -1,7 +1,7 @@
 // This file contains all the controls related to any user of the system
 const home = (req, res) => {
     console.log("Welcome to ElimuHub :)");
-    res.end("Welcome to ElimuHub :)");
+    res.send("Welcome to ElimuHub :)");
 }
 
 module.exports = {home}
