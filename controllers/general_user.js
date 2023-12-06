707d3d947a9fa057efe6b65cf94767dc955cbc99
myrajarenga <myrajarenga1234@.com>
2023-12-06 18:27:25 +0300

    changes to Login Implimentation

--- controllers/general_user.js
+++ controllers/general_user.js
@@ -55,7 +55,7 @@ const delete_user = async(req, res) => {
     const deletedUser = await User.findByIdAndDelete(userId);
 
     if (!deletedUser) {
-      return res.status(404).json({ message: 'User not found' });
+      return res.status(404).json({ message: 'User not found.' });
     }
 
     return res.status(200).json({ message: 'User deleted successfully' });
@@ -64,7 +64,5 @@ const delete_user = async(req, res) => {
     return res.status(500).json({ message: 'Internal server error' });
   }
 };
-
-
 
 module.exports = {home, delete_user, logIn};
