c5591704282cd95f664c6cae47ceeaf027b7b741
atonya-bravin <bravinatonya.brani@gmail.com>
2023-12-20 21:09:01 +0300

    Inclusion of the login route

--- routes/general.routes.js
+++ routes/general.routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
+const { home, delete_user, logIn, cliLogin } = require("../controllers/general_user.js");
 const { generateAndPopulateSession, checkSessionValidity} = require('../controllers/powerHouse.js');
 const session = require('express-session');
-const { home, delete_user } = require("../controllers/general_user.js");
 const {populateAuthKeys} = require("../controllers/populateInfo.js");
-
 // creation of a router object
 const router = express.Router();
+
 
 router.use(
     session({
@@ -15,12 +15,18 @@ router.use(
     })
 );
 
+router.use(express.json());
+
 // insert all the main routes associated with the general user under
+
 router.get("/", home);
 router.get('/startSession', generateAndPopulateSession);
 router.get('/sessionValidity', checkSessionValidity)
 router.delete("/delete_user/:userId", delete_user);
-router.get("/:apiKey", populateAuthKeys);
+router.get("/:apiKey", cliLogin);
+router.get("/populateAuthKey", populateAuthKeys)
+
+router.post("/login", logIn)
 
 module.exports = router;
 
